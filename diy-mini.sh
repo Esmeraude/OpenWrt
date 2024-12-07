10ac2535b98201fdee58525df38387f1601e0e3a
Esmeraude <34510490+Esmeraude@users.noreply.github.com>
2024-12-07 09:09:04 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -27,7 +27,7 @@ function git_sparse_clone() {
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
 
 # 科学上网插件
-git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+# git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
